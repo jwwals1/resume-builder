543980fdfd89a2c8c18cfc396c1a43a3b5917314
jwwals1 <joe_walsh59@yahoo.com>
2024-02-29 16:01:33 -0600

    added resume content

---
+++ src/Resume.jsx
@@ -0,0 +1,29 @@
+function Resume() {
+    return (
+        <div id="resume">
+            <div id="contactInfo">
+                <h2>Contact Information</h2>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <div id="educationInfor">
+                <h2>Education</h2>
+                <div>School Name:</div>
+                <div>Title of Graduation: </div>
+                <div>Date of Graduation: </div>
+            </div>
+            <div id="workHistoryInfo">
+                <h2>Work Information</h2>
+                <div>Job Title:</div>
+                <div>Job Title:</div>
+                <div>Job Details:</div>
+                <div>Start Date:</div>
+                <div>End Date:</div>
+            </div>
+        </div>
+    )
+}
+
+export default Resume
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import SideBar from './Sidebar.jsx'
+import Resume from './Resume.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <SideBar/>
+    <Resume/>
   </React.StrictMode>,
 )
